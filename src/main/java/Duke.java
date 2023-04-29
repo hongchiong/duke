e7d1e1e6068c70060a4a5b21e76ba0ca37d743b5
hongchiong <hongchiong@gmail.com>
2023-04-29 23:05:40 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner command = new Scanner(System.in);
+
+
+        while(true) {
+          System.out.println("\nDuke: What can I do for you?\n");
+  
+          String commandReceived = command.nextLine();
+  
+          if (commandReceived.equals("bye")) {
+            System.out.println("Duke: Bye. Hope to see you again soon!");
+            break;
+          }
+
+          System.out.println("Duke: " + commandReceived);
+      }
     }
 }
