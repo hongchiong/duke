e0558fc18869da99117665fa56516fa858e78697
hongchiong <hongchiong@gmail.com>
2023-04-29 23:44:23 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,7 +9,7 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        String[] userList = new String[100];
+        Task[] userList = new Task[100];
 
         Scanner command = new Scanner(System.in);
 
@@ -19,6 +19,22 @@ public class Duke {
   
           String commandReceived = command.nextLine();
   
+          String[] checkForMark = commandReceived.split(" ");
+
+          if (checkForMark[0].equals("mark")) {
+            int taskNumber = Integer.parseInt(checkForMark[1]) - 1;
+            userList[taskNumber].mark();
+            System.out.println("Nice! I've marked this task as done:\n" + "[" + userList[taskNumber].getStatusIcon() + "] " + userList[taskNumber].getDescription());
+            continue;
+          }
+
+          if (checkForMark[0].equals("unmark")) {
+            int taskNumber = Integer.parseInt(checkForMark[1]) - 1;
+            userList[taskNumber].unmark();
+            System.out.println("OK, I've marked this task as not done yet:\n" + "[" + userList[taskNumber].getStatusIcon() + "] " + userList[taskNumber].getDescription());
+            continue;
+          }
+
           if (commandReceived.equals("bye")) {
             System.out.println("Duke: Bye. Hope to see you again soon!");
             break;
@@ -29,14 +45,15 @@ public class Duke {
               if (userList[i] == null) {
                 break;
               };
-              System.out.println(i + 1 + ". " + userList[i]);
+              System.out.println(i + 1 + ". " + "[" + userList[i].getStatusIcon() + "] " + userList[i].getDescription());
             }
             continue;
           }
 
           for (int i = 0; i < userList.length; i++) {
             if (userList[i] == null) {
-              userList[i] = commandReceived;
+              Task t = new Task(commandReceived);
+              userList[i] = t;
               break;
             };
           }
